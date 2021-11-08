5b7fa12ae79ea869c02241ca9b9aa908d101bff3
[Jesse] <[jtenthije04@student.rocvantwente.nl]>
2021-11-08 11:51:16 +0100

    some updates

--- Test omgeving/2/UGOZ_Marcel_Roesink/CCSB/Models/Crv.cs
+++ Test omgeving/2/UGOZ_Marcel_Roesink/CCSB/Models/Crv.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,18 +12,34 @@ namespace CCSB.Models
     {
         [Key]
         public int? Id { get; set; }
+
         public enum Electricity
         {
+            [Description("Yes")]
             Yes = 0,
+
+            [Description("No")]
             No = 1
         }
+        [DisplayName("Merk")]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
         public string CrvName { get; set; }
+        [DisplayName("Type")]
         public string CrvType { get; set; }
+        [DisplayName("Lengte (in cm)")]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
         public int CrvLength { get; set; }
+        [DisplayName("Electriciteit")]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        [EnumDataType(typeof(Electricity))]
         public Electricity CrvElectricity { get; set; }
-        public string CrvPlate { get; set; }
 
+        [DisplayName("Kenteken")]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        public string CrvPlate { get; set; }
+        [DisplayName("Kies de gebruiker")]
         public string ApplicationUserId { get; set; }
+        [DisplayName("Gebruiker")]
         public ApplicationUser ApplicationUser { get; set; }
     }
 }
---
+++ Test omgeving/2/UGOZ_Marcel_Roesink/CCSB/Models/CrvAppointment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSB.Models
+{
+    public class CrvAppointment
+    {
+        [Key]
+        public int? Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string VehicleId { get; set; }
+        public bool IsAdminApproved { get; set; }
+        public string AdminId { get; set; }
+
+
+    }
+}
