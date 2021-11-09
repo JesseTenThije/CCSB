d10627aa94b2751961b134464f75b94906b0dc48
[Jesse] <[jtenthije04@student.rocvantwente.nl]>
2021-11-09 08:59:00 +0100

    added reserveringen
    missing vehicle dropdown

---
+++ Test omgeving/2/CCSB/CCSB/Controllers/ReserveringenController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CCSB.Models;
+
+namespace CCSB.Controllers
+{
+    public class ReserveringenController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReserveringenController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reserveringen
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Reserveringen.Include(r => r.ApplicationUser);
+            return View(await applicationDbContext.ToListAsync());
+           
+        }
+
+        // GET: Reserveringen/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserveringen = await _context.Reserveringen
+                .Include(r => r.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reserveringen == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserveringen);
+        }
+
+        // GET: Reserveringen/Create
+        public IActionResult Create()
+        {
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Reserveringen/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StartDatum,ApplicationUserId,Vehicle")] Reserveringen reserveringen)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reserveringen);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Email", reserveringen.ApplicationUserId);
+            return View(reserveringen);
+        }
+
+        // GET: Reserveringen/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserveringen = await _context.Reserveringen.FindAsync(id);
+            if (reserveringen == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Email", reserveringen.ApplicationUserId);
+            return View(reserveringen);
+        }
+
+        // POST: Reserveringen/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("Id,StartDatum,ApplicationUserId,Vehicle")] Reserveringen reserveringen)
+        {
+            if (id != reserveringen.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reserveringen);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReserveringenExists(reserveringen.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Email", reserveringen.ApplicationUserId);
+            return View(reserveringen);
+        }
+
+        // GET: Reserveringen/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserveringen = await _context.Reserveringen
+                .Include(r => r.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reserveringen == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserveringen);
+        }
+
+        // POST: Reserveringen/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var reserveringen = await _context.Reserveringen.FindAsync(id);
+            _context.Reserveringen.Remove(reserveringen);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReserveringenExists(int? id)
+        {
+            return _context.Reserveringen.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Test omgeving/2/CCSB/CCSB/Utility/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CCSB.Utility
+{
+    public static class Helper
+    {
+        public static readonly string Admin = "Beheerder";
+        public static readonly string User = "Gebuiker";
+        public static readonly string Vehicle = "Voertuig";
+
+        public static readonly string DropDownYes = "Ja";
+        public static readonly string DropDownNo = "Nee";
+
+        public static readonly string DropDownYesValue = "0";
+        public static readonly string DropDownNoValue = "1";
+
+        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
+        {
+            var items = new List<SelectListItem>
+            {
+                new SelectListItem{ Value=Helper.Admin , Text = Helper.Admin},
+                new SelectListItem{ Value=Helper.User , Text = Helper.User},
+            };
+            return items.OrderBy(s => s.Text).ToList();
+        }
+
+        public static List<SelectListItem> GetYesNoForDropDown()
+        {
+            var items = new List<SelectListItem>
+            {
+                new SelectListItem{ Value=Helper.DropDownYesValue , Text = Helper.DropDownYes},
+                new SelectListItem{ Value=Helper.DropDownNoValue , Text = Helper.DropDownNo},
+            };
+            return items.OrderBy(s => s.Text).ToList();
+        }
+    }
+}
+
