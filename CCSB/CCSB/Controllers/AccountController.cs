2eb7371549033ff0becc56f9c0230a82c6a84816
[Jesse] <[jtenthije04@student.rocvantwente.nl]>
2021-11-10 10:53:30 +0100

    made latest version from test to main

---
+++ CCSB/CCSB/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CCSB.Models;
+using CCSB.Utility;
+
+namespace CCSB.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager,
+        RoleManager<IdentityRole> roleManager )
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Reserveringen");
+                }
+                ModelState.AddModelError("", "Inloggen mislukt");
+            }
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Register()
+        {
+            if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult()) 
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.User));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(" ", error.Description);
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
