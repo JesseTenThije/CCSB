a5fbc4fee212b2edaf9de65f6ff9a84c19c5989e
Jelleol2001 <90192189+Jelleol2001@users.noreply.github.com>
2021-11-26 11:26:36 +0100

    postecode systeem

--- CCSB/CCSB/Migrations/20211126100455_adres.Designer.cs
+++ CCSB/CCSB/Migrations/20211126100455_adres.Designer.cs
@@ -4,14 +4,16 @@ using CCSB.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CCSB.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211126100455_adres")]
+    partial class adres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
